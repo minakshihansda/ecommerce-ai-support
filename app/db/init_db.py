9d9ff46be3ef9b948499eee913c6683e5bde2dd3
minakshihansda <minakshihansda089@gmail.com>
2026-09-07 20:17:05 +0530

    Add user model

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -7,6 +7,7 @@ from app.db.models.order import Order
 from app.db.models.order_item import OrderItem
 from app.db.models.support_ticket import SupportTicket
 from app.db.models.category import Category
+from app.db.models.user import User
 
 PRODUCTS = [
     {
